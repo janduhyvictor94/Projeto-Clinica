0d17965340cfc450f4886a2263a48f0e3f25dcc2
janduhyvictor94 <janduhy123@gmail.com>
2025-12-05 18:22:39 -0300

    Recriando arquivo supabase.js perdido

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '@/supabase'; // <-- MUDANÇA PRINCIPAL: Usando Supabase
+import { supabase } from '@/supabase.js';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import PageHeader from '@/components/ui/PageHeader';
 import { Button } from '@/components/ui/button';
