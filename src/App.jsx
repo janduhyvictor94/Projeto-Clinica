1172159e969f8835b1da9b8db3d06889ba05cd7b
janduhyvictor94 <janduhy123@gmail.com>
2025-12-05 17:53:56 -0300

    Configurando QueryClientProvider no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Criando a configuração do React Query
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
+    // Envolvendo o site inteiro com o Provedor
+    <QueryClientProvider client={queryClient}>
       <Pages />
       <Toaster />
-    </>
+    </QueryClientProvider>
   )
 }
 
-export default App 
+export default App
