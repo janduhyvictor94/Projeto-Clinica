9866d5d77b6e74164a8f79ed1105ef9a276d99bf
janduhyvictor94 <janduhy123@gmail.com>
2025-12-05 17:32:26 -0300

    Corrigindo import do utils

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { 
   LayoutDashboard, 
   Users, 
